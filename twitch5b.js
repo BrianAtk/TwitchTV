1784689a09cd846311019fb26023414b486ee921
Brian Atkins <brian_tkins@yahoo.com>
2015-10-06 11:13:13 -0700

    Update twitch5b.js
    Added ability to perform searches on subscribers.

--- twitch5b.js
+++ twitch5b.js
@@ -10,10 +10,13 @@ $.each(subscribers, function (index, member) {
             getStream(d, member);
         })
       .fail(function (error) {
-
-          var notAvail = "http://i173.photobucket.com/albums/w71/brian_atkins1/Not_available_icon.jpg"
-          hlink = "<img src='" + notAvail + "'  id='twitchLogo'>" + member + ":" + error.statusText
-          $(".messages").append("<div class='cardSize black'><br/>" + hlink + "</div>");
+          if (member) {
+              var notAvail = "http://i173.photobucket.com/albums/w71/brian_atkins1/Not_available_icon.jpg"
+              hlink = "<img src='" + notAvail + "'  id='twitchLogo'>" + member + ":" + error.statusText
+              $(".messages").append("<div class='cardSize black " + member + "'>" + "<br/>" + hlink + "</div>");
+          } else {
+              $(".messages").append("<div class='cardSize black " + member + "'>" + "<br/>" + "***WARNING - Subscriber list contains a blank subscriber" + "</div>");
+          }
       })
 
 });
@@ -31,9 +34,8 @@ function getStream(d, member) {
         .fail(function (error) {
             // User is no longer streaming
             notAvail = "http://i173.photobucket.com/albums/w71/brian_atkins1/Old-antique-sign-on-doorway-that-says-gone-for-good.jpg"
-
-            hlink = "<img src='" + notAvail + "'  id='twitchLogo'>" + d.display_name + ":no longer streaming"
-            $(".messages").append("<div class='cardSize black'><br/>" + hlink + "</div>");
+            hlink = "<img src='" + notAvail + "' class='twitchLogo'" + ">" + d.display_name + ":no longer streaming"
+            $(".messages").append("<div class='cardSize black " + member + "'>" + "<br/>" + hlink + "</div>");
         })
 };
 
@@ -44,24 +46,67 @@ function appendRecord(streams, d, member) {
     if (streams.stream != null) {
         // this user is online streaming now.  Display his streaming information
 
-        hlink = "<img src='" + streams.stream.channel.logo + "' id='twitchLogo'>" + "<span class='black'><a target='_blank' href='http://www.twitch.tv/" + member + "'>" +
+        hlink = "<img src='" + streams.stream.channel.logo + "' class='twitchLogo'>" + "<span class='black'><a target='_blank' href='http://www.twitch.tv/" + member + "'>" +
                   d.display_name + "</a>" + " - Viewers: " + streams.stream.viewers + " Playing:" + streams.stream.game + "</span>";
-        $(".online").append("<div class='cardSize'>" + " <br/>" + hlink + "</div>");
+
+        $(".online").append("<div class='cardSize black " + member + "'>" + "<br/>" + hlink + "</div>");
 
     } else {
         // this user is not streaming now.  Display his profile information
 
         if (imgSrc !== null) {
-            hlink = "<img src='" + imgSrc + "'  id='twitchLogo'>" + "<a target ='_blank' href= 'http://www.twitch.tv/" + d.display_name + "'>" + d.display_name + "</a>";
+            hlink = "<img src='" + imgSrc + "'  class='twitchLogo'>" + "<a target ='_blank' href= 'http://www.twitch.tv/" + d.display_name + "'>" + d.display_name + "</a>";
         } else {
             // no logo available.  Display default no icon logo
 
             notAvail = "http://i173.photobucket.com/albums/w71/brian_atkins1/Not_available_icon.jpg"
-            hlink = "<img src='" + notAvail + "'  id='twitchLogo'>" + "<a target ='_blank' href= 'http://www.twitch.tv/" + member + "'>" + d.display_name + "</a>";
+            hlink = "<img src='" + notAvail + "'  class='twitchLogo'>" + "<a target ='_blank' href= 'http://www.twitch.tv/" + member + "'>" + d.display_name + "</a>";
         }
-        $(".offline").append("<div class='cardSize'><br/>" + hlink + "<div>");
+
+
+        $(".offline").append("<div class='cardSize black " + member + "'>" + "<br/>" + hlink + "</div>");
     }
+
 };
 
 
+function doSearch() {
+    var searchString = $(".searchText").val();
 
+    if (searchString) {
+        // loop through the entire list of users and either hide or show them based on what they are searching for
+        $.each(subscribers, function (index, member) {
+            whichId = "." + member;
+            // see if this member matches the search string
+            if (member.indexOf(searchString) >= 0) {
+
+                $(whichId).show();
+            } else {
+                $(whichId).hide();
+
+            }
+        });
+
+
+    } else {
+        $(".textMessage").text("Please enter search string");
+    }
+
+}
+
+
+$('.searchButton').click(function () {
+    $(".textMessage").text("");
+    doSearch();
+});
+
+$('.clearButton').click(function () {
+    $(".searchText").val("");
+    $(".textMessage").text("");
+    $.each(subscribers, function (index, member) {
+        whichId = "." + member;
+        $(whichId).show();
+
+    });
+    
+});
